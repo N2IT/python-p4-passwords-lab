f117796ce6033ccd4b1095154a81e86c0985359e
Tony Eder <tonyeder11@gmail.com>
2024-05-02 10:33:13 -0700

    dunzo

--- server/app.py
+++ server/app.py
@@ -16,7 +16,6 @@ class ClearSession(Resource):
         return {}, 204
 
 class Signup(Resource):
-    
     def post(self):
         json = request.get_json()
         user = User(
@@ -28,16 +27,46 @@ class Signup(Resource):
         return user.to_dict(), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict(),200
+        
+        else:
+            return {}, 204
 
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json() # retrieves un and pw from JSON request
+        username = data['username'] 
+        password = data['password']
+
+        user = User.query.filter(User.username == username).first()
+        # executes query to confirm user exists
+        
+        if user is None:
+            return {'error': 'Invalid username or password'}, 401
+
+        # checks if pw is correct using 'authenticate' method in user instance
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+
+        return {'error': 'Invalid username or password'}, 401
+
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {}, 204
+
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
